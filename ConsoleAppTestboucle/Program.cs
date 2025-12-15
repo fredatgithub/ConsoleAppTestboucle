2b2f5d074d4ecb05531da2b66b6ae6adb36ad945
Freddy JUHEL <PFJL06651@commun.ad.sncf.fr>
2025-12-15 17:17:53 +0100

    clean code

--- ConsoleAppTestboucle/Program.cs
+++ ConsoleAppTestboucle/Program.cs
@@ -9,16 +9,17 @@ namespace ConsoleAppTestboucle
     static void Main()
     {
       // This is a simple console application that demonstrates a loop
-      // It will print numbers from 1 to 10
+      int end = 400_000;
       Stopwatch chrono = new Stopwatch();
       chrono.Start();
-      for (int i = 1; i <= 400_000; i++)
+      for (int i = 1; i <= end; i++)
       {
-        for (int j = i; j <= 400_000; j++)
+        for (int j = i; j <= end; j++)
         {
           Console.WriteLine($"Number: {i} and number j: {j}");
         }
       }
+
       chrono.Stop();
       // print the elapsed time in seconds, minutes, and hours
       var elapsedInSeconds = $"Elapsed time: {chrono.Elapsed.TotalSeconds} seconds";
@@ -31,7 +32,7 @@ namespace ConsoleAppTestboucle
 
       try
       {
-        using (StreamWriter sw = new StreamWriter("boucle400K.txt"))
+        using (var sw = new StreamWriter("boucle400K.txt"))
         {
           sw.WriteLine("To loop from 1 to 400_000 in i and then in j from i to 400k, it takes");
           sw.WriteLine(elapsedInSeconds);
